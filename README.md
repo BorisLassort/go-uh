c16c23cf6b774c997174fc8ecbf022fccafa71de
Boris Lassort <163381+BorisLassort@users.noreply.github.com>
2020-07-10 23:06:47 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,28 @@
+go-uhelpers
+===================
+
+Package uhelpers (micro helpers) provides very tiny and simple helpers functions
+
+## Examples
+
+```go
+package main
+
+import (
+	"fmt"
+	uh "github.com/borislassort/go-uhelpers"
+)
+
+func main() {
+	// Download
+	err := uh.DownloadFile("file.zip", "https://the.u.r.l/file.zip")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err := uh.Unzip("file.zip", "dir/")
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+```
---
+++ uhelpers.go
@@ -0,0 +1,89 @@
+// Package uhelpers (micro helpers) provides very tiny and simple helpers functions.
+package uhelpers
+
+import (
+	"archive/zip"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+// DownloadFile download a file from a direct URL
+func DownloadFile(outfilePath string, url string) error {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	// Create the file
+	out, err := os.Create(outfilePath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
+
+// Unzip extract zip archive to a directory
+func Unzip(zipPath string, dirPath string) error {
+
+	r, err := zip.OpenReader(zipPath)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := r.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	os.MkdirAll(dirPath, 0755)
+
+	extractAndWriteFile := func(f *zip.File) error {
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := rc.Close(); err != nil {
+				panic(err)
+			}
+		}()
+
+		path := filepath.Join(dirPath, f.Name)
+
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(path, f.Mode())
+		} else {
+			os.MkdirAll(filepath.Dir(path), f.Mode())
+			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer func() {
+				if err := f.Close(); err != nil {
+					panic(err)
+				}
+			}()
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	for _, f := range r.File {
+		err := extractAndWriteFile(f)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
